274b19d6fa0994bec0efe2a9c062b9fe2a5d061b
ataher-elae-dh <ataher.elae@gmail.com>
2025-10-08 18:42:33 +0600

    'd'

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
